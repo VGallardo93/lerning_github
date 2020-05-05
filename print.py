bc52e22784fee1497bb3a59058c047e75f3a1ac8
VGallardo93 <victorgallardo023@gmail.com>
2020-05-05 02:27:17 -0400

    print modified

--- print.py
+++ print.py
@@ -3,3 +3,13 @@
 # Where there's a will, there's a way...
 
 print("\n>>>\tWelcome. This is my first git reporitory.\t<<<\n")
+
+'''
+A basic repository about some python scripts.
+
+I don't know to use Git and Github but I'm lerning.
+
+So, I created this repository to practice.
+
+PD: I'm beginner speaker xd.
+'''
