9ed80d6992559ba52ec2180ad706ac44dc372524
VGallardo93 <victorgallardo023@gmail.com>
2020-05-10 00:41:24 -0400

    test.py v1.1 +.vscode folder

--- Test.py
+++ Test.py
@@ -1,3 +1,19 @@
 # This is a test file in Python 3
 
 print('Welcome to the new file... By vg.\n')
+
+name_ = input('Hi. Insert your name: ')
+
+while True:
+  if name_.isdigit():
+    print('\nInvalid name. Please try again.')
+    name_ = input('Insert your name: ')
+    continue
+
+  else:
+    print('\nHi {name_}, nice to meet you.', name_)
+    break
+
+print('That\'s all folk!, bye!')
+
+
