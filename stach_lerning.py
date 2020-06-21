39fa938cf948da7a5588fec8ab3aada26b470914
VGallardo93 <victorgallardo023@gmail.com>
2020-06-20 22:31:51 -0400

    stash-py nueva clase

--- stach_lerning.py
+++ stach_lerning.py
@@ -1,11 +1,24 @@
 print('Este es un archivo que debemos guardar')
 print('PERO NO EN UN COMMIT')
-<<<<<<< Updated upstream
-print()
-=======
 print()
 
 """
 ESTA LINEAS ES AGREGADA EN EL WORKING DIRECTORY
 """
->>>>>>> Stashed changes
+class User():
+  def __init__(self
+              ,run_user = None
+              ,name = None
+              ,address = None
+              ,phone1 = None
+              ,phone2 = None
+              ,email1 = None
+              ,email2 = None):
+    
+    self.run_user = run_user
+    self.name = name
+    self.address = address
+    self.phone1 = phone1
+    self.phone2 = phone2
+    self.email1 = email1
+    self.email2 = email2
