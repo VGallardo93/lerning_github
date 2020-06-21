4c572e38edc6d06fad2092fd9ee38523c0497a57
VGallardo93 <victorgallardo023@gmail.com>
2020-06-20 22:18:24 -0400

    stash_script add

---
+++ stach_lerning.py
@@ -0,0 +1,11 @@
+print('Este es un archivo que debemos guardar')
+print('PERO NO EN UN COMMIT')
+<<<<<<< Updated upstream
+print()
+=======
+print()
+
+"""
+ESTA LINEAS ES AGREGADA EN EL WORKING DIRECTORY
+"""
+>>>>>>> Stashed changes
